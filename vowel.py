054bc39690ae167835f20f191de0539a3308fb0c
kanishwarikandhasamy <43762254+kanishwarikandhasamy@users.noreply.github.com>
2019-05-23 14:26:56 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,5 @@
+kani=input()
+if(kani=='a'or kani=='e'or kani=='i'or kani=='o'or kani=='u'or kani=='A'or kani=='E'or kani=='I'or kani=='O'or kani=='U'):
+    print("Vowel")
+else:
+    print("Consonant")
