02411752657604e53b10c1cf2120bdfd041bff8f
kanishwarikandhasamy <43762254+kanishwarikandhasamy@users.noreply.github.com>
2019-05-23 15:14:22 +0530

    Update greatest.py

--- greatest.py
+++ greatest.py
@@ -1,9 +1,9 @@
-kani=int(input())
-suji=int(input())
-rubi=int(input())
-if((kani>=suji)and (kani>=rubi)):
-    print(kani)
-elif((rubi>=kani)and (rubi>=suji)):
-    print(rubi)
+k=int(input())
+s=int(input())
+r=int(input())
+if((k>=s)and (k>=r)):
+    print(k)
+elif((r>=k)and (r>=s)):
+    print(r)
 else:
-    print(suji)
+    print(s)
