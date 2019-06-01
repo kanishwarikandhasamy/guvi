024f835d90f1d2ee20e1361c40d3c7c70c1cdb0a
kanishwarikandhasamy <43762254+kanishwarikandhasamy@users.noreply.github.com>
2019-06-01 11:22:55 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,6 +1,9 @@
 kani=int(input())
-if((kani%2==0)or(kani%3==0)or(kani%5==0)or(kani%7==0)or(kani%9==0)or(kani%11==0)):
-    print("no")
+count=0
+for i in range(1,kani+1):
+    if(kani%i==0):
+        count=count+1
+if(count>2):
+        print("no")
 else:
-    print("yes")
-
+        print("yes")
