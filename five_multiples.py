62bab3dbf23c7d7f4c050e87ae6c317db453bc4d
kanishwarikandhasamy <43762254+kanishwarikandhasamy@users.noreply.github.com>
2019-06-01 23:11:49 +0530

    Update five_multiples.py

--- five_multiples.py
+++ five_multiples.py
@@ -1,6 +1,12 @@
 k=int(input())
 r=k*6
 s=0
-for i in range(k,r,k):
-    print(i,end=' ')
+if(k!=0):
+    for i in range(k,r,k):
+      print(i,end=' ')
+else:
+    for i in range(0,5):
+        print(k,end=" ")
+
+
 
