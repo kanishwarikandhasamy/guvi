f828f30420e8de78e58b0a8674ae2a137d448205
kanishwarikandhasamy <43762254+kanishwarikandhasamy@users.noreply.github.com>
2019-06-02 08:06:42 +0530

    Update sum_arrays.py

--- sum_arrays.py
+++ sum_arrays.py
@@ -1,10 +1,9 @@
 n=int(input())
 k=int(input())
-lst=[]
-for i in range(1,n+1):
-    s=int(input())
-    lst.append(k)
 sum=0
-for i in range(1,k+1):
-    sum=sum+i
-print(sum)
+if(k<=n):
+    for i in range(1,k+1):
+        sum=sum+i
+    print(sum)
+else:
+    print("invalid")
